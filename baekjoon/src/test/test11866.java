9f724c47781724d7bb26d02d9ff47d01f276a348
rhj1216 <rhj1216@gmail.com>
2022-10-13 17:44:48 +0900

    10.13

---
+++ baekjoon/src/test/test11866.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class test11866 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Queue<Integer> q = new LinkedList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		sb.append("<");
+		for (int i = 1; i <= N; i++) {
+			q.offer(i); // 1234순으로 넣기
+		}
+		while (!q.isEmpty()) {
+			// K-1번 앞에 있는 원소를 뒤로 보낸다.
+			for (int i = 0; i < K - 1; i++) {
+				q.offer(q.poll());
+			}
+			if (q.size() == 1)
+				sb.append(q.poll());
+			else
+				sb.append(q.poll() + ", ");
+		}
+		sb.append(">");
+		System.out.println(sb.toString());
+	}
+}
--- baekjoon/src/test/test5086.java
+++ baekjoon/src/test/test5086.java
@@ -1,9 +1,11 @@
+package test;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main {
+public class test5086{
 	public static void main(String[] args) throws IOException {
 		String f = "factor\n";
 		String m = "multiple\n";
