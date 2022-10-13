c8c8784f1165c1ffc99ec683d88f271d1d146526
rhj1216 <rhj1216@gmail.com>
2022-10-13 17:44:58 +0900

    10.13

--- baekjoon/src/test/test11866.java
+++ baekjoon/src/test/test11866.java
@@ -1,14 +1,11 @@
 package test;
-
 import java.util.Queue;
 import java.util.StringTokenizer;
 import java.util.LinkedList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
 public class test11866 {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Queue<Integer> q = new LinkedList<>();
