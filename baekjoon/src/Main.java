32230ee7c8216362e4c40706ddaebdaa5de3be29
rhj1216 <rhj1216@gmail.com>
2022-10-25 08:58:08 +0900

    1025

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,30 +1,60 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int sum = Integer.MAX_VALUE; // 초기 상태 여부 확인을 위한 값으로 설정
-		StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
+		StringTokenizer st;
 
-		while (sub.hasMoreTokens()) {
-			int temp = 0;
-			// 빼기로 숫자들구분하여 더해줄것 그후 빼기진행해야 최솟값
-			StringTokenizer plus = new StringTokenizer(sub.nextToken(), "+");
-			while (plus.hasMoreTokens()) {
-				temp += Integer.parseInt(plus.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[6];
+		int MaxW1 = 0, MaxW2 = 0, MaxH1 = 0, MaxH2 = 0;
+		int d=0;
+		for (int i = 0; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			d = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
+
+			// 가장 긴 가로의 길이와 위치 찾기
+			if ((d == 1 || d == 2) && MaxW1 < arr[i]) {
+				MaxW1 = arr[i];
+				MaxW2 = i;
 			}
-			// 가장 앞의수는 무조건 양수
-			if (sum == Integer.MAX_VALUE) {
-				sum = temp;
-			} else {
-				sum -= temp;
+			// 가장 긴 세로의 길이와 위치 찾기
+			else if ((d == 3 || d == 4) && MaxH1 < arr[i]) {
+				MaxH1 = arr[i];
+				MaxH2 = i;
 			}
 		}
-		System.out.println(sum);
-	}
 
+		int right, left, minWidth, minHeight;
+
+		if (MaxW2 + 1 == 6)
+			right = 0;
+		else
+			right = MaxW2 + 1;
+
+		if (MaxW2 - 1 == -1)
+			left = 5;
+		else
+			left = MaxW2 - 1;
+		// 빈 사각형의 세로 길이
+		minHeight = Math.abs(arr[right] - arr[left]);
+
+		if (MaxH2 + 1 == 6)
+			right = 0;
+		else
+			right = MaxH2 + 1;
+
+		if (MaxH2 - 1 == -1)
+			left = 5;
+		else
+			left = MaxH2 - 1;
+		// 빈 사각형의 가로 길이
+		minWidth = Math.abs(arr[right] - arr[left]);
+
+		System.out.println(((MaxW1 * MaxH1) - (minHeight * minWidth)) * N);
+	}
 }
