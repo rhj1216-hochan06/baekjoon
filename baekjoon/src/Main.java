7a97db666454bc227ec23c172922db28d6141c62
rhj1216 <rhj1216@gmail.com>
2022-10-28 15:40:49 +0900

    123

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -6,5 +6,32 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
